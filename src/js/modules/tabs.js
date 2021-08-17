aadd7e6c2f77738cc537c379f5ae8a22aafdf14c
vv_tomilin <backatom1@gmail.com>
2021-08-17 21:49:32 +0500

    Less 4 (Работа с табами (вкладками) на странице)

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,46 @@
+/* eslint no-param-reassign: "error" */
+const tabs = (
+  headerSelector,
+  tabSelector,
+  contentSelector,
+  activeClass,
+) => {
+  const header = document.querySelector(headerSelector);
+  const tab = document.querySelectorAll(tabSelector);
+  const content = document.querySelectorAll(contentSelector);
+
+  const hideTabContent = () => {
+    content.forEach((item) => {
+      item.style.display = 'none';
+    });
+
+    tab.forEach((item) => {
+      item.classList.remove(activeClass);
+    });
+  };
+
+  const showTabContent = (i = 0) => {
+    content[i].style.display = 'block';
+    tab[i].classList.add(activeClass);
+  };
+
+  hideTabContent();
+  showTabContent(0);
+
+  header.addEventListener('click', (e) => {
+    const { target } = e;
+
+    if (target
+        && (target.classList.contains(tabSelector.replace(/\./, ''))
+        || target.parentNode.classList.contains(tabSelector.replace(/\./, '')))) {
+      tab.forEach((item, i) => {
+        if (target === item || target.parentNode === item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+};
+
+export default tabs;
